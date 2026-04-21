c8c6e9df8167c913aae0ab3b8e2dc4ed7689ef57
lulca-a <contato.luca.luca@gmail.com>
2026-04-21 13:03:55 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,64 @@
+function gerar_troca(){
+    let troca ={
+        troca_id : 123,
+        
+        pessoa_1:document.getElementById('pessoa1').value,
+        dia_1:document.getElementById('dia1').value,
+        turno_1:document.getElementById('turno1').value,
+        pessoa_2:document.getElementById('pessoa2').value,
+        dia_2:document.getElementById('dia2').value,
+        turno_2:document.getElementById('turno2').value
+    }
+    for (let chave in troca){
+        if (!troca[chave]){
+            return;
+        }
+    }
+    return troca;
+};
+
+
+let form = document.querySelector('form');
+
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    let botao = document.getElementById('botao')
+    botao.disabled = true;
+    botao.style.backgroundColor = 'grey'
+    let troca = gerar_troca()
+
+    
+    if (!troca){
+        alert("Dados inválidos!")
+        botao.disabled = false;
+        botao.style.backgroundColor = '#333'
+        return
+    }else{
+        console.log(troca);
+
+        fetch('http://127.0.0.1:5500/troca',{
+            method:'POST',
+            headers:{"Content-Type": "application/json"},
+            body:JSON.stringify(troca)
+        
+        }).then(function(resposta){
+            if (!resposta.ok){
+                throw new Error('ERRO');
+            }else{        
+                return resposta.json();
+            }    
+        
+        }).then(function(dados){
+            console.log(dados)
+
+            alert('Dados enviados com sucesso')
+            form.reset()
+            botao.disabled = false
+            botao.style.backgroundColor = '#333'
+        
+        }).catch(function(erro){
+            alert('erro de envio')
+            botao.disabled = false
+            botao.style.backgroundColor = '#333'
+        })
+    }});
---
+++ backend.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post('/troca')
+print(/troca)
+def gerar_troca():
+    def enviar_emails():
+        ...
+    def salvar():
+        ...
+
