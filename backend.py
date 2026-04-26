849970822ae51061cfdba5d94dca73f1bc36c631
lulca-a <contato.luca.luca@gmail.com>
2026-04-26 20:26:32 +0000

    Refactor troca function and add salvar_troca
    The function 'salvar_troca' was added to save the exchange data into a list, and the 'troca' function was updated to call 'salvar_troca' and include the 'id_troca' in the response.

--- backend.py
+++ backend.py
@@ -3,18 +3,26 @@ from flask_cors import CORS
 
 pessoas = ['João','Maria','Carlos']
 campos  =['pessoa_1','pessoa_2','dia_1','dia_2','turno_1','turno_2']
+
 app = Flask(__name__)
 CORS(app)
+lista = []
+
 @app.route('/troca',methods=['POST'])
+def salvar_troca(dados):
+    dados['id_troca'] = len(lista)+1
+    lista.append(dados)
 
 def troca():
     dados = request.json
     for campo in campos:
         if not dados.get(campo):
             return jsonify({'mensagem' : f'{campo} não preenchido'})
-                       
+            
+    salvar_troca(dados)
+    
     return jsonify({
-            'id_troca':123,
+            'id_troca':dados['id_troca'],
             'pessoa_1':dados['pessoa_1'],
             'dia_1':dados['dia_1'], 
             'turno_1':dados['turno_1'],
@@ -22,5 +30,4 @@ def troca():
             'dia_2':dados['dia_2'],
             'turno_2':dados['turno_2']
         })
-
 app.run(debug= True)
